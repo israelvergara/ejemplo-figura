da707f32db30f4a2b47b58b3570640009b1bd992
Israel Vergara <israel.vergara102824@gmail.com>
2017-09-10 21:39:53 -0500

    Initial sharing of project

---
+++ Figura.java
@@ -0,0 +1,25 @@
+/**
+ * Supercalse 
+ * @author Israel Vergara
+ * @version 8 Sep 2017
+ */
+
+public class Figura
+{
+    protected float area;
+    
+    public Figura()
+    {
+        area = 0;
+    }
+    
+    public void calculaArea()
+    {
+        System.out.println("No se como cacular el area");
+    }
+    
+    public void imprimeArea()
+    {
+        System.out.println("El area de la figura es: "+area);
+    }
+}
